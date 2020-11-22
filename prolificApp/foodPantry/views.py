3eb81d7969f59645b8628aef1c16508592a12bea
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2020-11-21 21:14:35 -0500

    added comments to views.py

--- prolificApp/foodPantry/views.py
+++ prolificApp/foodPantry/views.py
@@ -8,12 +8,12 @@ def index():
 @foodPantry_app.route('/about')
 def about():
 	return render_template('About.html')
-
+#creates endpoint for the About webpage 
 @foodPantry_app.route('/directory')
 def directory():
 	return render_template('Directory.html')
-
+#creates endpoint for the directory webpage 
 @foodPantry_app.route('/food pantry profile')
 def fpProfile():
 	return render_template('FoodPantryProfile.html')
-
+#creates endpoint for the food pantry profile page webpage 
